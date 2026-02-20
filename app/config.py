106b21a83d31393c35d4ffbd07aae0aa272f9742
Fabian Omoke <40634286+FabianOmoke@users.noreply.github.com>
2026-02-20 15:42:57 +0000

    updated reqs and key

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+SUPABASE_URL = os.getenv("https://vwwjhhdbsldnevpccpmj.supabase.co")
+SUPABASE_SERVICE_KEY = os.getenv("sb_publishable_1tUU7Yjw9oOwYHOcwbMQIg_nhyjEpDY")
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-LASTFM_API_KEY = os.getenv("LASTFM_API_KEY")
+LASTFM_API_KEY = os.getenv("791e35407d0dba66f434eb0949eda95a")
